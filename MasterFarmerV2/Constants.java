325ff080f32a549bd1f2c37cc7fd57ba03c4dcf6
Sesshom93 <40584048+Sesshom93@users.noreply.github.com>
2018-07-01 11:43:53 -0400

    Create Constants.java

---
+++ MasterFarmerV2/Constants.java
@@ -0,0 +1,71 @@
+package MasterFarmer;
+
+
+import org.powerbot.script.Tile;
+
+public class Constants {
+
+    //Player Animations
+    public static final int PICKPOCKET = 881;
+    public static final int STUNNED = 424;
+    public static final int GUARD_ATTACKING = 401;
+
+    //Npc ID's
+    public static final int MASTER_FARMER = 3257;
+    public static final int GUARD = 3259;
+
+    //Item ID's
+
+    public static final int DODGY_NECKLACE = 21143;
+
+    //Seed ID's
+    public static final int POTATO_SEED = 5318;
+    public static final int ONION_SEED = 5319;
+    public static final int CABBAGE_SEED = 5324;
+    public static final int TOMATO_SEED = 5322;
+    public static final int SWEETCORN_SEED = 5320;
+    public static final int STRAWBERRY_SEED = 5323;
+    public static final int WATERMELON_SEED = 5321;
+    public static final int BARLEY_SEED = 5305;
+    public static final int HAMMERSTONE_SEED = 5307;
+    public static final int ASGARNIAN_SEED = 5308;
+    public static final int JUTE_SEED = 5306;
+    public static final int YANILLIAN_SEED = 5309;
+    public static final int KRANDORIAN_SEED = 5310;
+    public static final int WILDBLOOD_SEED = 5311;
+    public static final int MARIGOLD_SEED = 5096;
+    public static final int NASTURTIUM_SEED = 5098;
+    public static final int ROSEMARY_SEED = 5097;
+    public static final int WOAD_SEED = 5099;
+    public static final int LIMPWURT_SEED = 5100;
+    public static final int REDBERRY_SEED = 5101;
+    public static final int CADAVABERRY_SEED = 5102;
+    public static final int DWELLBERRY_SEED = 5103;
+    public static final int JANGERBERRY_SEED = 5104;
+    public static final int WHITEBERRY_SEED = 5105;
+    public static final int POISON_IVY_SEED = 5106;
+    public static final int GUAM_SEED = 5291;
+    public static final int MARRENTILL_SEED = 5292;
+    public static final int TARROMIN_SEED = 5293;
+    public static final int HARRALANDER_SEED = 5294;
+    public static final int RANARR_SEED = 5295;
+    public static final int TOADFLAX_SEED = 5296;
+    public static final int IRIT_SEED = 5297;
+    public static final int AVANTOE_SEED = 5298;
+    public static final int KWUARM_SEED = 5299;
+    public static final int SNAPDRAGON_SEED = 5300;
+    public static final int CADANTINE_SEED = 5301;
+    public static final int LANTADYME_SEED =5302;
+    public static final int DWARF_WEED_SEED = 5303;
+    public static final int TORSTOL_SEED = 5304;
+    public static final int MUSHROOM_SPORE = 5282;
+    public static final int BELLADONNA_SEED = 5281;
+    public static final int CACTUS_SEED = 5280;
+
+    //Food ID's
+    public static final int TROUT = 333;
+
+    //Path ID's
+    public static final Tile[] PATH_TO_BANK = {new Tile(3077, 3250, 0), new Tile(3080, 3250, 0), new Tile(3083, 3250, 0), new Tile(3087, 3250, 0), new Tile(3090, 3249, 0), new Tile(3092, 3246, 0), new Tile(3092, 3243, 0)};
+    public static final Tile[] PATH_TO_FARMER = {new Tile(3092, 3243, 0), new Tile(3092, 3246, 0), new Tile(3089, 3247, 0), new Tile(3086, 3247, 0), new Tile(3083, 3247, 0), new Tile(3080, 3250, 0)};
+}
